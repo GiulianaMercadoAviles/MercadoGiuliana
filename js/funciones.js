a6cc4e0a15b96979071d6e27394de4cf832bd5f9
GiulianaMercado <giulianamercado43@gmail.com>
2024-04-04 21:24:51 -0300

    agregue botones y funciones en js

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,19 @@
-alert("hola mundo")
+function saludar(nombre) {
+    alert("Hola " + nombre)
+}
+
+function resultado(numero1, numero2) {
+    alert("el resultado es: " + (numero1 + numero2))
+}
+
+function numero(n) {
+    const aux = 72;
+
+    if(aux > n) {
+        alert ("el número " + n +" es menor que el auxiliar " + aux)
+    }
+    else if(aux < n) {
+        alert ("el número " + n + " es mayor que el auxiliar " + aux)
+    }
+}
+
